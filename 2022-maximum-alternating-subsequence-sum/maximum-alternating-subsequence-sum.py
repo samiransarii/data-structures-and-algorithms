3a93d37f809d743e6ffb069bd0468cabb15570be
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-07 02:15:47 -0600

    Time: 1094 ms (47.61%) | Memory: 37.2 MB (47.19%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
@@ -9,10 +9,10 @@ class Solution:
 
         for i in range(1, N+1):
 
-            # Even length subsequence till index i means the index will be odd so subtract
+            # Even length subsequence till i-term means the index will be odd so subtract
             dp[i][0] = max(dp[i-1][1] - nums[i-1], dp[i-1][0])
 
-            # Odd length subsequence till index i means the index will be even so add
+            # Odd length subsequence till i-th means the index will be even so add
             dp[i][1] = max(dp[i-1][0] + nums[i-1], dp[i-1][1])
 
         output = max(dp[N][0], dp[N][1])
