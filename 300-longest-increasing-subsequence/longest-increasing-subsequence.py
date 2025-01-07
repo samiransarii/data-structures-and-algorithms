73ef829a652148e4a61959d5627fb9bdae5c795d
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-07 03:33:46 -0600

    Time: 2630 ms (9.49%) | Memory: 18.2 MB (18.13%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -4,9 +4,9 @@ class Solution:
         dp = [1] * N
 
         maxLIS = 1
-        for i in range(N - 1, -1, -1):
-            for j in range(i+1, N):
-                if nums[j] > nums[i]:
+        for i in range(N):
+            for j in range(i):
+                if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j] + 1)
                     maxLIS = max(maxLIS, dp[i])
 
