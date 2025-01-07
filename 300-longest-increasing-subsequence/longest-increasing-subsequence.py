a35841ea6108a8070ccdb915ce9c8ecc106c6dbf
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-07 03:29:33 -0600

    Time: 2618 ms (9.62%) | Memory: 17.8 MB (37.10%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.py
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.py
@@ -1,22 +1,13 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         N = len(nums)
-        dp = [-1] * (N)
+        dp = [1] * N
 
-        def solve(idx):
-            if idx == N:
-                return 0
+        maxLIS = 1
+        for i in range(N):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    maxLIS = max(maxLIS, dp[i])
 
-            if dp[idx] != -1:
-                return dp[idx]
-
-            max_len = 1
-            for next_idx in range(idx + 1, N):
-                if nums[next_idx] > nums[idx]:
-                    max_len = max(max_len, 1 + solve(next_idx))
-
-            dp[idx] = max_len
-            return dp[idx]
-
-        # Compute the LIS considering all possible starting points
-        return max(solve(i) for i in range(N))
+        return maxLIS
