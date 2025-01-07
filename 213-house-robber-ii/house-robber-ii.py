d48d216afea6216aa44c979aee95607a96aaaa7d
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-07 00:06:31 -0600

    Time: 0 ms (100.00%) | Memory: 18 MB (5.61%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -5,29 +5,24 @@ class Solution:
         if N == 1:
             return nums[0]
 
-        dp = [0] * (N + 1)
-        dp[0] = 0
+        dp = [0] * N
+        dp[0] = nums[0]
 
-        for i in range(1, N):
-            steal = nums[i - 1] + (dp[i - 2] if i - 2 >= 0 else 0)
+        for i in range(1, N-1):
+            steal = nums[i] + (dp[i - 2] if i - 2 >= 0 else 0)
             skip = dp[i - 1]
-
             dp[i] = max(steal, skip)
 
-        output1 = dp[N-1]
+        output1 = dp[N-2]
 
-        dp = [0] * (N + 1)
-        dp[0] = 0
+        dp = [0] * N
+        dp[1] = nums[1]
 
-        for i in range(2, N+1):
-            steal = nums[i - 1] + (dp[i - 2] if i - 2 >= 0 else 0)
+        for i in range(2, N):
+            steal = nums[i] + (dp[i - 2] if i - 2 >= 0 else 0)
             skip = dp[i - 1]
-
             dp[i] = max(steal, skip)
 
-        output2 = dp[N]
+        output2 = dp[N-1]
 
         return max(output1, output2)
-
-
-        
