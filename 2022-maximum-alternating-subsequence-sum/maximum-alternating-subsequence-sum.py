4b3f37939ee643e0bf363cd2c82290abf3e1aca7
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-07 02:12:54 -0600

    Time: 1111 ms (46.75%) | Memory: 37.5 MB (45.24%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.py
@@ -2,28 +2,18 @@ class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         N = len(nums)
         
-        dp = [[-1] * 2 for _ in range(N)]
+        # dp[[-1, -1]] for even and odd length subsequence
+        # dp[idx][0] for even length
+        # dp[idx][1] for odd length
+        dp = [[0] * 2 for _ in range(N + 1)]
 
-        # flag -> True means even idx (Boolean: 1)
-        # flag -> False means odd idx (Boolean: 0)
-        def solve(idx, flag):
-            if idx >= N:
-                return 0
+        for i in range(1, N+1):
 
-            if dp[idx][flag] != -1:
-                return dp[idx][flag]
+            # Even length subsequence till index i means the index will be odd so subtract
+            dp[i][0] = max(dp[i-1][1] - nums[i-1], dp[i-1][0])
 
-            # skip the current element
-            skip = solve(idx + 1, flag)
+            # Odd length subsequence till index i means the index will be even so add
+            dp[i][1] = max(dp[i-1][0] + nums[i-1], dp[i-1][1])
 
-            val = nums[idx]
-            val = val if flag else -val
-
-            take = solve(idx + 1, not flag) + val
-
-            dp[idx][flag] = max(skip, take)
-            return dp[idx][flag]
-
-        output = solve(0, True)
+        output = max(dp[N][0], dp[N][1])
         return output
-
