525427715eda69457e79e0c00a1540a24bf052d4
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-08 00:50:17 -0600

    Time: 718 ms (24.99%) | Memory: 18.1 MB (43.66%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.py
+++ 1129-longest-string-chain/longest-string-chain.py
@@ -1,40 +1,34 @@
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
         N = len(words)
-        sortedWords = sorted(words, key=len)
-        dp = [[''] * (N + 1) for _ in range(N)]
+        words = sorted(words, key=len)
+
+        dp = [1] * N
 
         def isPredecessor(wordA, wordB):
             lenA, lenB = len(wordA), len(wordB)
+
             if lenA + 1 != lenB:
                 return False
 
             a, b = 0, 0
+            mismatched = False
             while b < lenB and a < lenA:
                 if wordA[a] == wordB[b]:
                     a += 1
-                b += 1
-            return a == lenA
+                    b += 1
+                elif wordA[a] != wordB[b]:
+                    if mismatched:
+                        return False
+                    mismatched = True
+                    b += 1
+            return True
         
-        def solve(idx, prev):
-            if idx >= N:
-                return 0
+        longestStringChain = 1
+        for i in range(N):
+            for j in range(i):
+                if isPredecessor(words[j], words[i]):
+                    dp[i] = max(dp[i], dp[j] + 1)
+                    longestStringChain = max(longestStringChain, dp[i])
 
-            if dp[idx][prev] != '':
-                return dp[idx][prev]
-
-            wordA = sortedWords[prev]
-            wordB = sortedWords[idx]
-            take = 0
-            if prev == -1 or isPredecessor(wordA, wordB):
-                take = 1 + solve(idx + 1, idx)
-            
-            skip = solve(idx + 1, prev)
-
-            if prev != -1:
-                dp[idx][prev] = max(take, skip)
-            return dp[idx][prev] if prev != -1 else max(take, skip)
-
-        output = solve(0, -1)
-        return output
-
+        return longestStringChain
