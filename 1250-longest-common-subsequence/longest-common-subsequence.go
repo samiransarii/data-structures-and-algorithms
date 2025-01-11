a3bd51b8fcbea0dcc4fccae6a3a406e967bd9093
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-11 00:41:44 -0600

    Time: 40 ms (16.78%) | Memory: 21.3 MB (21.61%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.go
+++ 1250-longest-common-subsequence/longest-common-subsequence.go
@@ -13,25 +13,22 @@ func longestCommonSubsequence(text1 string, text2 string) int {
     // Define the recursive function to find LCS
     var findLCS func(p1 int, p2 int) int
     findLCS = func(p1 int, p2 int) int {
-        // Return 0 if indx is out of bound
-        // for any of the string
+        // Base case: if either index is out of bounds
         if p1 >= N1 || p2 >= N2 {
             return 0
         }
 
+        // If characters match, move both pointers forward
         if dp[p1][p2] != -1 {
             return dp[p1][p2]
         }
 
-        // If current Indx matches for both the strings
-        // add 1 and explore other indxs
+        // If characters match, move both pointers forward
         if text1[p1] == text2[p2] {
             dp[p1][p2] = 1 + findLCS(p1 + 1, p2 + 1)
             return dp[p1][p2]
         } else {
-            // Else explore both the possiblities of skipping one
-            // Idx from both string one by one and return the max
-            // answer.
+            // Otherwise, try both skipping options
             dp[p1][p2] = max(findLCS(p1 + 1, p2), findLCS(p1, p2 + 1))
             return dp[p1][p2]
         }
