439be2a1fcd85985cb4a6b568df61ea2d81675a9
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-11 22:14:03 -0600

    Time: 0 ms (100.00%) | Memory: 7.5 MB (41.33%) - LeetSync

--- 72-edit-distance/edit-distance.go
+++ 72-edit-distance/edit-distance.go
@@ -3,45 +3,24 @@ func minDistance(word1 string, word2 string) int {
     N := len(word2)
 
     // Initialize dp array
-    var dp = make([][]int, M)
+    var dp = make([][]int, M+1)
     for i := range dp {
-        dp[i] = make([]int, N)
-        for j := range dp[i] {
-            dp[i][j] = -1
-        }
+        dp[i] = make([]int, N+1)
     }
 
-    var findMinDistance func(i int, j int) int
-    findMinDistance = func(i int, j int) int {
-        // Base case := word1 if finished insert the remaining characters from word2
-        if i >= M {
-            return N - j
-        // Base case2 := word2 if finished delete the remaining characters from word1
-        } else if j >= N {
-            return M - i
+    for i := 0; i <= M; i ++ {
+        for j := 0; j <= N; j++ {
+            if i == 0 || j == 0 {
+                dp[i][j] = i+j
+
+            } else if word1[i-1] == word2[j-1] {
+                dp[i][j] = dp[i-1][j-1]
+
+            } else {
+                dp[i][j] = 1 + min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
+            }
         }
+    } 
 
-        if dp[i][j] != -1 {
-            return dp[i][j]
-        }
-
-        // If charcters matched solve for the next characters
-        if word1[i] == word2[j] {
-            dp[i][j] = findMinDistance(i + 1, j + 1)
-            return dp[i][j]
-        
-        // If characters differes, explore all the possibilities of
-        // Inserting, deleting, and replacing
-        } else {
-            insert := 1 + findMinDistance(i, j + 1)
-            delete := 1 + findMinDistance(i + 1, j)
-            replace := 1 + findMinDistance(i + 1, j + 1)
-
-            dp[i][j] = min(insert, delete, replace)
-            return dp[i][j]
-        }
-    }
-
-    output := findMinDistance(0, 0)
-    return output
+    return dp[M][N]
 }
