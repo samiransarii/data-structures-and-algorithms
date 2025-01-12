66a8a48182b92d98383141346f328f072b3a3d0e
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-12 01:19:42 -0600

    Time: 31 ms (18.35%) | Memory: 18.9 MB (5.99%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.go
+++ 647-palindromic-substrings/palindromic-substrings.go
@@ -1,29 +1,42 @@
 func countSubstrings(s string) int {
     N := len(s)
 
+    dp := make([][]int, N+1)
+    for i := range dp {
+        dp[i] = make([]int, N+1)
+            for j := range dp[i] {
+                dp[i][j] = -1
+            }
+    }
+
     // Check if palindromic string
-    var isPalindrome func(i int, j int) bool
-    isPalindrome = func(i int, j int) bool {
+    var isPalindrome func(i int, j int) int
+    isPalindrome = func(i int, j int) int {
         if i > j {
-            return true
+            return 1
+        }
+
+        if dp[i][j] != -1 {
+            return dp[i][j]
         }
         
         if s[i] == s[j] {
-            return isPalindrome(i+1, j-1)
+            dp[i][j] = isPalindrome(i+1, j-1)
+            return dp[i][j]
         }
 
-        return false
+        return 0
     }
 
     // Find all the substrings -> bruteforce
     count := 0
     for i := 0; i < N; i++ {
         for j := i; j < N; j++ {
-            if isPalindrome(i, j) {
+            if isPalindrome(i, j) == 1 {
                 count ++
             }
         }
     } 
-
-   return count
+    
+    return count
 }
