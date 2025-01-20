05744df73db52f700026e9d11652bbec67d9751c
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-19 21:44:14 -0600

    Time: 905 ms (29.93%) | Memory: 17.8 MB (37.31%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.py
+++ 526-beautiful-arrangement/beautiful-arrangement.py
@@ -1,17 +1,9 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
         count = 0
-
-        def isBeautifulArrangement(currpath):
-            for idx, num in enumerate(currpath):
-                if (idx + 1) % num != 0 and num % (idx + 1) != 0:
-                    return False
-
-            return True
        
         def findPermutations(pos, visited):
             nonlocal count
-
             if pos > n:
                 count += 1
                 return
