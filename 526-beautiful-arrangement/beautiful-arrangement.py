cd5fe1e327aecde2ac8c9b5deda2c085e777e635
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-19 23:47:14 -0600

    Time: 278 ms (79.66%) | Memory: 31.4 MB (5.31%) - LeetSync

--- 526-beautiful-arrangement/beautiful-arrangement.py
+++ 526-beautiful-arrangement/beautiful-arrangement.py
@@ -1,21 +1,27 @@
 class Solution:
     def countArrangement(self, n: int) -> int:
         count = 0
+        memo = {}
        
         def findPermutations(pos, visited):
             nonlocal count
             if pos > n:
-                count += 1
-                return
+                return 1
 
+            state = (pos, str(visited))
+            if state in memo:
+                return memo[state]
+
+            count = 0
             for num in range(1, n + 1):
                 if num not in visited and (pos % num == 0 or num % pos == 0):
                     visited.add(num)
-                    findPermutations(pos + 1, visited)
+                    count += findPermutations(pos + 1, visited)
                     visited.remove(num)
-                    
+
+            memo[state] = count
+            return count
 
         findPermutations(1, set())
         return count
-
         
