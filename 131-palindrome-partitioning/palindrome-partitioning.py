0dcbe6b8947a2b1be861ad5c8e55a476a93acfcb
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-20 00:24:12 -0600

    Time: 55 ms (38.87%) | Memory: 34.5 MB (55.95%) - LeetSync

---
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -0,0 +1,22 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        n = len(s)
+        output = []
+
+        def isPalindrome(string):
+            return string == string[::-1]
+
+        def solve(idx, currpath):
+            if idx >= n:
+                output.append(currpath[:])
+            
+            for i in range(idx, n):
+                substring = s[idx:i+1]
+                if isPalindrome(substring):
+                    currpath.append(substring)
+                    solve(i + 1, currpath)
+                    currpath.pop()
+
+
+        solve(0, [])
+        return output
