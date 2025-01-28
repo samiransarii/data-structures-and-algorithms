7353b6601865f853dc46cdfbec4dc10c806f4ad5
Samir Ansari <76509419+samiransarii@users.noreply.github.com>
2025-01-28 12:51:40 -0600

    Time: 10 ms (22.61%) | Memory: 8.8 MB (62.61%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.go
@@ -0,0 +1,49 @@
+func findDiagonalOrder(mat [][]int) []int {
+    m := len(mat)
+    n := len(mat[0])
+    output := []int{}
+    diagonals := map[int][]int{}
+
+    // Store the diagonal elements in the map
+    for i := 0; i < m; i++ {
+        for j := 0; j < n; j++ {
+            diagonals[i+j] = append(diagonals[i+j], mat[i][j])
+        }
+    }
+
+    keys := make([]int, 0, len(diagonals))
+    for key := range diagonals {
+        keys = append(keys, key)
+    }
+    // Sort the keys
+    sort.Ints(keys)
+
+    // Store the diagonal element in the output
+    flip := true
+    for _, key:= range keys {
+        values := diagonals[key]
+        if flip {
+            reverseValues := reverse(values)
+            output = append(output, reverseValues...)
+
+        } else {
+            output = append(output, values...)
+        }
+        
+        // Toggle flip
+        flip = !flip
+    }
+
+    return output
+}
+
+func reverse(slice []int) []int {
+    left, right := 0, len(slice)-1
+    for left < right {
+        slice[left], slice[right] = slice[right], slice[left]
+        left++
+        right--
+    }
+
+    return slice
+}
